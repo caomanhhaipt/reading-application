43fffab1c53c775fb37edb5ed1c61c95bc8eccfe
caomanhhaipt <caomanhhaipt@gmail.com>
2018-05-20 22:11:35 +0700

    Add two rule

---
+++ KimDung/Rule10.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class Rule10 : Rule
+    {
+        public override bool IsValid(string str)
+        {
+            string vowel = "AÁÀẢẠÃ"
+                + "ĂẮẰẶẲẴ"
+                + "ÂẤẦẨẬẪ"
+                + "EÉÈẺẸẼ"
+                + "ÊẾỀỂỆỄ"
+                + "IÍÌỈỊĨ"
+                + "OÓÒỎỌÕ"
+                + "ÔỐỒỔỘỖ"
+                + "ƠỚỜỞỢỠ"
+                + "UÚÙỦỤŨ"
+                + "ƯỨỪỬỰỮ"
+                + "YÝỲỶỴỸ";
+            string consonant = "BDĐKLMPQSTVX";
+            string tmp = str.ToUpper();
+
+            String formatStr = "";
+
+            for(int i = 0; i < tmp.Length; i++)
+            {
+                if(vowel.Contains("" + tmp[i]) || consonant.Contains("" + tmp[i]))
+                {
+                    formatStr += tmp[i];
+                }
+            }
+
+            for(int i = 1; i < formatStr.Length - 1; i++)
+            {
+                if(consonant.Contains("" + formatStr[i]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override void Explain()
+        {
+            Console.WriteLine("Một từ tiếng việt, các phụ âm không được phép đứng giữa" +
+                "B, D, Đ, K, L, M, P, Q, S, T, V, X");
+        }
+    }
+}
---
+++ KimDung/Rule9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class Rule9 : Rule
+    {
+        public override bool IsValid(string str)
+        {
+            string consonant = "BCDĐGHKLMNPQRSTVX";
+            string tmp = str.ToUpper();
+
+            int countConsonant = 0;
+            for(int i = 0; i < tmp.Length; i++)
+            {
+                if(consonant.Contains("" + tmp[i]))
+                {
+                    countConsonant += 1;
+                }
+
+                if (countConsonant > 5)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public override void Explain()
+        {
+            Console.WriteLine("Một từ tiếng việt có tối đa 5 nguyên âm");
+        }
+    }
+}
